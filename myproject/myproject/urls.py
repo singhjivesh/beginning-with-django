5f545ecb1c1398c331338ed18163c34ed4a70931
singhjivesh <s.jivesh1996@gmail.com>
2018-01-03 11:51:12 +0530

    updated urls.py

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -24,3 +24,29 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
+
+'''
+Now we have to tell Django when to serve this view. It’s done inside the urls.py file:
+
+urls.py
+
+from django.conf.urls import url
+from django.contrib import admin
+
+from boards import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^admin/', admin.site.urls),
+]
+
+If you compare the snippet above with your urls.py file, you will notice I added the following new line: url(r'^$', views.home, name='home') and imported the views module from our app boards using from boards import views.
+
+As I mentioned before, we will explore those concepts in great detail later on.
+
+But for now, Django works with regex to match the requested URL. For our home view, I’m using the ^$ regex, which will match an empty path, which is the homepage (this url: http://127.0.0.1:8000). If I wanted to match the URL http://127.0.0.1:8000/homepage/, my url would be: url(r'^homepage/$', views.home, name='home').
+
+Let’s see what happen:
+
+python manage.py runserver
+'''
