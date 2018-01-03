15e8bffca60ee1dedfe5cfbd1fa84fbb4e8c05b0
singhjivesh <s.jivesh1996@gmail.com>
2018-01-04 00:15:39 +0530

    updated

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,3 +1,6 @@
+
+
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
@@ -5,17 +8,3 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('Hello, World!')
 
- '''
-views.py
-
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('Hello, World!')
-
-Views are Python functions that receive an HttpRequest object and returns an HttpResponse object. Receive a request as a parameter and returns a response as a result. That’s the flow you have to keep in mind!
-
-So, here we defined a simple view called home which simply returns a message saying Hello, World!.
-
-Now we have to tell Django when to serve this view. It’s done inside the urls.py file:
-'''
