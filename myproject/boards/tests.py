d0f352e1ada6f0027b51ad3f26987da251f53f53
singhjivesh <s.jivesh1996@gmail.com>
2018-01-09 21:22:25 +0530

    added test commands for boards

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -1,14 +1,18 @@
-
-
-
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.urls import resolve
 from django.test import TestCase
-from .views import home, board_topics
-from .models import Board
+from .views import home
 
 class HomeTests(TestCase):
-    # ...
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
+
 
 class BoardTopicsTests(TestCase):
     def setUp(self):
