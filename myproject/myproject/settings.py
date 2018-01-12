a24df9eb029fa6ea13333eba98a6f8b2861c3729
singhjivesh <s.jivesh1996@gmail.com>
2018-01-12 10:10:32 +0530

    added two installed features named as widgets_tweaks and accounts added logout and login redirectional url to home

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'widget_tweaks',
     
+    'accounts',
     'boards',
 ]
 
@@ -126,3 +127,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+LOGOUT_REDIRECT_URL = 'home'
+
+LOGIN_REDIRECT_URL = 'home'
